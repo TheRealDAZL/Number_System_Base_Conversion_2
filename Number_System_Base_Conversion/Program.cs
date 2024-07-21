b9d8e9eacd129f7a72d55a4c569750f835d54e26
therealdazl <louis.theriault@outlook.fr>
2024-07-21 17:39:39 -0400

    Beta version. I modified my code and did extensive testing and found that converting a number from a prime number base to another prime number base makes the results more imprecise. This issue seems related to "Machine Epsilon", and I don't know of any fix for that problem yet.

--- Number_System_Base_Conversion/Program.cs
+++ Number_System_Base_Conversion/Program.cs
@@ -1,32 +1,91 @@
-﻿namespace Number_System_Base_Conversion
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace Number_System_Base_Conversion
 {
     internal class Program
     {
         static void Main()
         {
-            const string initialBaseMsg = "Enter the initial base to convert from. The base must be an integer from 2 to 36:";
+            const string initialMsg = "***** Number System Base Converter, for any integer base from to 2 to 36 *****\n\n" +
+                    "IMPORTANT NOTE: due to \"Machine Epsilon\" related issues, I have limited the precision of my algorithm, so " +
+                    "that the expansion of a floating-point number is truncated after 15 rounds. So the computed values are merely approximative.";
+            const string finalMsg = "\nDo you want to continue? Hit Enter to continue, or write \"q\" and then press Enter to quit:";
+            const string initialBaseMsg = "\nEnter the initial base to convert from. The base must be an integer from 2 to 36:";
             const string finalBaseMsg = "\nEnter the final base to convert to. The base must be an integer from 2 to 36:";
             const string numberMsg = "\nEnter the number you wish to convert:";
+
             char[] digitsLibrary = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
                 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
             bool quit = false;
 
+
+
+            Console.WriteLine(initialMsg);
+
             while (!quit)
             {
-                int initialBase = ValidateBase(initialBaseMsg);
+                ConvertNumber();
 
-                int finalBase = ValidateBase(finalBaseMsg);
+                Console.WriteLine(finalMsg);
 
-                ConvertNumber(initialBase, finalBase, numberMsg);
-
-                quit = QuitOrNot();
+                quit = ((Console.ReadLine() == "q") ? true : false);
                 Console.Write("\n");
             }
 
 
 
 
+
+            void ConvertNumber()
+            {
+                int initialBase = ValidateBase(initialBaseMsg);
+                int finalBase = ValidateBase(finalBaseMsg);
+                string input = "";
+                bool numberIsNegative = false;
+                int positionDotOrComma = -1;
+                double value = 0;
+
+                while (true)
+                {
+                    Console.WriteLine(numberMsg);
+                    input = Console.ReadLine().ToUpper();
+
+                    if (input.StartsWith('-'))
+                    {
+                        input = input.Substring(1);
+                        numberIsNegative = true;
+                    }
+
+                    else if (input.StartsWith("+"))
+                    {
+                        input = input.Substring(1);
+                    }
+
+                    if (ValidateInput(input, out positionDotOrComma)
+                        && ConvertStringToValue(initialBase, input, positionDotOrComma, out value))
+                    {
+                        break;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("\nError: invalid value.\n");
+                    }
+                }
+
+                string output = ConvertValueToString(finalBase, value);
+
+                if (input.Contains(","))
+                {
+                    output = output.Replace(".", ",");
+                }
+
+                output = (numberIsNegative ? "-" + output : output);
+
+                Console.WriteLine($"\nHere is the converted number:\n{output}");
+            }
 
             int ValidateBase(string message)
             {
@@ -50,60 +109,13 @@
                 return number;
             }
 
-            void ConvertNumber(int initialBase, int finalBase, string message)
+            bool ValidateInput(string input, out int positionDotOrComma)
             {
-                string input = "";
-                bool numberIsNegative = false;
-                double value = 0;
-
-                while (true)
-                {
-                    Console.WriteLine(message);
-                    input = Console.ReadLine().ToUpper();
-
-                    if (ValidateThenCompute(initialBase, input, out value, out numberIsNegative))
-                    {
-                        break;
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("\nError: invalid value.\n");
-                    }
-                }
-
-                string output = ComputeThenConvert(finalBase, value);
-
-                if (input.Contains(","))
-                {
-                    output = output.Replace(".", ",");
-                }
-
-                output = (numberIsNegative ? "-" + output : output);
-
-                Console.WriteLine($"\nHere is the converted number:\n{output}");
-            }
-
-            bool ValidateThenCompute(int initialBase, string input, out double value, out bool numberIsNegative)
-            {
-                numberIsNegative = false;
-                value = 0;
-                int positionDotOrComma = -1;
+                positionDotOrComma = -1;
 
                 if (input == "")
                 {
                     return false;
-                }
-
-                if (input.StartsWith('-'))
-                {
-                    input = input.Substring(1);
-                    numberIsNegative = true;
-                }
-                
-                else if (input.StartsWith("+"))
-                {
-                    input = input.Substring(1);
                 }
 
                 if (!(input.Contains(".") && input.Contains(",")) && (input.Contains(".") || input.Contains(",")))
@@ -128,6 +140,12 @@
                     return false;
                 }
 
+                return true;
+            }
+
+            bool ConvertStringToValue(int initialBase, string input, int positionDotOrComma, out double value)
+            {
+                value = 0;
                 int exponent = (positionDotOrComma == -1 ? 0 : positionDotOrComma + 1 - input.Length);
 
                 for (int position = input.Length - 1; position >= 0; position--)
@@ -137,19 +155,16 @@
                     char c = input[position];
                     int tempValue = -1;
 
-                    for (int counter = 0; counter < initialBase; counter++)
+                    int index = Array.IndexOf(digitsLibrary, c, 0, initialBase);
+
+                    if (index != -1)
                     {
-                        if (digitsLibrary[counter] == c)
-                        {
-                            tempValue = counter;
+                        tempValue = index;
+                    }
 
-                            break;
-                        }
-
-                        else if (counter == initialBase - 1)
-                        {
-                            return false;
-                        }
+                    else
+                    {
+                        return false;
                     }
 
                     value += tempValue * Math.Pow(initialBase, exponent);
@@ -159,7 +174,7 @@
                 return true;
             }
 
-            string ComputeThenConvert(int finalBase, double value)
+            string ConvertValueToString(int finalBase, double value)
             {
                 string output = "";
                 double integerPart = Math.Truncate(value);
@@ -171,7 +186,7 @@
 
                     while (fractionPart > 0 && precisionLimit < 15)
                     {
-                        int integerValue = (int) Math.Floor(fractionPart * finalBase);
+                        int integerValue = (int) Math.Truncate(fractionPart * finalBase);
                         output += digitsLibrary[integerValue];
                         fractionPart = fractionPart * finalBase - integerValue;
 
@@ -188,19 +203,12 @@
 
                 while (integerPart > 0)
                 {
-                    int residue = (int) integerPart % finalBase;
+                    int residue = (int) (integerPart % finalBase);
                     output = digitsLibrary[residue] + output;
                     integerPart = Math.Floor(integerPart / finalBase);
                 }
 
                 return output;
-            }
-
-            bool QuitOrNot()
-            {
-                Console.WriteLine("\nDo you want to continue? Hit Enter to continue, or write \"q\" and then press Enter to quit:");
-
-                return ((Console.ReadLine() == "q") ? true : false);
             }
         }
     }
